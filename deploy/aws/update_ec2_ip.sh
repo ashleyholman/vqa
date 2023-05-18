de85da5cf760d19bdf49c2050f5fb62434e09c6c
Ashley Holman + ChatGPT <ash@macbook>
2023-05-18 08:11:00 +0700

    aws/deploy/update_ec2_ip.sh: Add parameter to specify cpu or gpu ASG

--- deploy/aws/update_ec2_ip.sh
+++ deploy/aws/update_ec2_ip.sh
@@ -2,9 +2,32 @@
 STACK_NAME="vqa-stack"
 EC2_IP_FILE=$( dirname -- "$0"; )/../../EC2_IP_ADDRESS
 
+# Check if the correct number of command line arguments were provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <cpu|gpu>"
+  exit 1
+fi
+
+# Get instance type from command line argument
+INSTANCE_TYPE=$1
+
+# Check if the instance type argument is valid
+if [ "$INSTANCE_TYPE" != "cpu" ] && [ "$INSTANCE_TYPE" != "gpu" ]; then
+  echo "Invalid instance type. Must be 'cpu' or 'gpu'."
+  exit 1
+fi
+
+# Determine the cloudformation output key that will provide the ASG name based on the instance type
+if [ "$INSTANCE_TYPE" == "cpu" ]; then
+  OUTPUT_KEY="CPUOnlyAutoScalingGroupName"
+else
+  OUTPUT_KEY="GPUAutoScalingGroupName"
+fi
+
+
 echo == Describing stack $STACK_NAME..
 # Name of your Auto Scaling Group
-ASG_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CPUOnlyAutoScalingGroupName'].OutputValue" --output text)
+ASG_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='$OUTPUT_KEY'].OutputValue" --output text)
 echo Found ASG $ASG_NAME
 
 echo == Retrieving instance ID from ASG..
