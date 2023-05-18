eef5e3c82158a9b7fe20a986a79404f4dbcea621
Ashley Holman + ChatGPT <ash@macbook>
2023-05-18 08:48:36 +0700

    scripts/fetch_datasets.py: On EC2, download from S3 mirror if available

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
+        'boto3',
         'torch',
         'transformers',
         'pillow',
+        'matplotlib',
         'numpy',
         'tqdm'
     ],
--- src/scripts/fetch_datasets.py
+++ src/scripts/fetch_datasets.py
@@ -2,6 +2,68 @@ import os
 import zipfile
 import requests
 from tqdm import tqdm
+import boto3
+import socket
+import subprocess
+from botocore.exceptions import ClientError
+
+import requests
+
+# Define the URLs of the data to be downloaded
+URLS = [
+    # Training set
+    "http://images.cocodataset.org/zips/train2014.zip",
+    "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip",
+    "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip",
+
+     # Validation set
+     "http://images.cocodataset.org/zips/val2014.zip",
+     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip",
+     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip"
+]
+
+def is_ec2():
+    return True
+    """Check if we're on an EC2 instance"""
+    try:
+        # Make a request to the EC2 metadata service
+        response = requests.get('http://169.254.169.254/latest/meta-data/instance-id', timeout=0.02)
+        # If the request succeeds without throwing an exception, we're on an EC2 instance
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        # If the request throws an exception, we're likely not on an EC2 instance
+        return False
+
+
+def file_exists_in_s3(s3_url):
+    """Check if file exists in S3"""
+    s3 = boto3.resource('s3')
+    bucket_name = s3_url.split('/')[2]
+    s3_key = '/'.join(s3_url.split('/')[3:])
+    bucket = s3.Bucket(bucket_name)
+
+    objs = list(bucket.objects.filter(Prefix=s3_key))
+    return len(objs) > 0 and objs[0].key == s3_key
+
+def download_file_from_s3(s3_url, target_path):
+    """Download a file from S3 using AWS CLI"""
+    bucket_name = s3_url.split('/')[2]
+    s3_key = '/'.join(s3_url.split('/')[3:])
+
+    # Use AWS CLI instead of boto3 here, to get the nice progress bar in the output.
+    try:
+        print(f"Using s3 mirror for {s3_key}...")
+        subprocess.check_call([
+            'aws',
+            's3',
+            'cp',
+            f's3://{bucket_name}/{s3_key}',
+            target_path
+        ])
+        return True
+    except subprocess.CalledProcessError:
+        # The download failed
+        return False
 
 def download_file(url, target_path):
     print(f"Downloading {url}...")
@@ -31,29 +93,29 @@ print(f"Data dir: {data_dir}")
 # Create the data directory if it does not exist
 os.makedirs(data_dir, exist_ok=True)
 
-# Define the URLs of the data to be downloaded
-urls = [
-    # Training set
-    #"http://images.cocodataset.org/zips/train2014.zip",
-    "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip",
-    "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip",
-
-     # Validation set
-     "http://images.cocodataset.org/zips/val2014.zip",
-     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip",
-     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip"
-]
+# Check if we're on EC2
+on_ec2 = is_ec2()
 
 # Iterate over each URL
-for url in urls:
+for url in URLS:
     # Parse the filename
     filename = os.path.join(data_dir, url.split("/")[-1])
     
     # Download the file if it does not already exist
     if not os.path.exists(filename):
-        download_file(url, filename)
+        # If on EC2, try to download from the S3 mirror.
+        # Not all files are on the mirror necessarily, so we'll fall back to the
+        # original source if it's not there.
+        s3_mirror_url = f"s3://vqa/datasets/{filename.split('/')[-1]}"
+        if on_ec2 and file_exists_in_s3(s3_mirror_url):
+            download_file_from_s3(s3_mirror_url, filename)
+            pass
+        else:
+            # If not on EC2 or if the file doesn't exist on S3, download from the original URL
+            download_file(url, filename)
     else:
         print(f"{filename} already exists, skipping download.")
         
     # Extract the zip file
     extract_zip(filename, data_dir)
+
