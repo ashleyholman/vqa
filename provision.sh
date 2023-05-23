b5dbb62847651234caa20f19659a054e46690c0f
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-05-23 21:24:30 +0700

    provision.sh: Specify 8 dataloader workers

--- provision.sh
+++ provision.sh
@@ -16,5 +16,4 @@ python src/scripts/fetch_datasets.py
 echo "==> Make mini dataset"
 ./src/scripts/make_mini_dataset.sh
 echo "==> Enter work loop"
-nohup python src/scripts/do_work.py >> train.log & 2>&1
-
+nohup python src/scripts/do_work.py --num-dataloader-workers 8 >> train.log & 2>&1
