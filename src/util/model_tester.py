23c8bd86a8733a253fd15079e5c7c79d8b740ef8
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-06-02 15:47:20 +0700

    ModelTester: A class to contain what test_model.py used to do standalone.

---
+++ src/util/model_tester.py
@@ -0,0 +1,43 @@
+import torch
+
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from torch.nn.functional import cross_entropy
+
+class ModelTester:
+    def __init__(self, config, dataset, num_dataloader_workers):
+        self.config = config
+        self.dataset = dataset
+
+        # Create a DataLoader to handle batching of the dataset
+        self.dataloader = DataLoader(dataset, batch_size=config.batch_size, num_workers=num_dataloader_workers, shuffle=config.shuffle)
+
+    def test(self, model, performance_tracker, device, no_progress_bar=False):
+        # Move model to evaluation mode
+        model.eval()
+
+        # Wrap data_loader with tqdm to show a progress bar, unless --no-progress-bar was specified
+        dataloader = self.dataloader
+        if not no_progress_bar:
+            dataloader = tqdm(dataloader)
+
+        # No need to track gradients for this
+        with torch.no_grad():
+            for idx, batch in enumerate(dataloader, start=1):
+                # Transfer data to the appropriate device
+                question_embeddings = batch["question_embedding"].to(device)
+                image_embeddings = batch["image_embedding"].to(device)
+                labels = batch["label"].to(device)
+
+                # Run the model and get the predictions
+                logits = model(image_embeddings, question_embeddings)
+
+                # Calculate the loss
+                loss = cross_entropy(logits, labels)
+
+                # Update the performance tracker
+                if performance_tracker:
+                    performance_tracker.update_metrics(logits, labels, loss.item())
+                if idx % 500 == 0:
+                    print(f"\nBatch {idx}..")
