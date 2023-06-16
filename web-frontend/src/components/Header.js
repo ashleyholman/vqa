3f416ed0f4bd85b8cd061f998aaf24c7bf71b044
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-06-16 15:31:46 +0700

    web: Move header inline css to .css file and and update styling

--- web-frontend/src/components/Header.js
+++ web-frontend/src/components/Header.js
@@ -1,17 +1,14 @@
+import React from 'react';
 import githubMark from '../svg/github-mark-white.svg';
+import './Header.css';
+import BebasNeueRegular from '../fonts/BebasNeue-Regular.ttf';
 
 function Header() {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '10px',
-      background: 'linear-gradient(90deg, rgba(58,123,213,1) 0%, rgba(58,96,115,1) 50%, rgba(58,123,213,1) 100%)'
-    }}>
-      <h1 style={{ color: 'white' }}>Visual Question Answering</h1>
-      <a href="https://github.com/ashleyholman/vqa">
-        <img src={githubMark} alt="github" style={{ width: '30px', height: '30px' }} />
+    <div className="header-container">
+      <h1 className="header-title">Visual Question Answering</h1>
+      <a href="https://github.com/ashleyholman/vqa" className="github-link">
+        <img src={githubMark} alt="github" className="github-mark" />
       </a>
     </div>
   );
