f60250b2e08566df8a65bee9406f31d1448eded6
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-06-17 00:28:24 +0700

    generate_web_json.py: Add --max-runs argument
    Instead of hard-coding it to 10.

--- src/scripts/generate_web_json.py
+++ src/scripts/generate_web_json.py
@@ -9,6 +9,7 @@ from src.util.run_manager import RunManager
 
 def main():
     parser = argparse.ArgumentParser(description='Fetch data from DynamoDB and plot the graph.')
+    parser.add_argument('--max-runs', type=int, default=10, help='Maximum number of runs to fetch')
     parser.add_argument('--regenerate-existing', action='store_true', help='Regeneral JSON files for existing runs')
 
     args = parser.parse_args()
@@ -16,7 +17,7 @@ def main():
     graph_generator = GraphGenerator()
 
     run_manager = RunManager()
-    recent_runs = run_manager.get_recent_runs(10)
+    recent_runs = run_manager.get_recent_runs(args.max_runs)
     json_files = graph_generator.generate_json_files(recent_runs, args.regenerate_existing)
 
     print(f'Results written to JSON files to {json_files}.\n')
