d6b0b631e1b13398656a4cb517e9d3c18323cf99
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-06-21 08:49:33 +0700

    model: Update gating mechanism to use both modalities for each of the gating weights.

--- src/models/gated_multi_model_unit.py
+++ src/models/gated_multi_model_unit.py
@@ -4,20 +4,19 @@ import torch
 class GatedMultiModalUnit(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
-
         self.image_gate = nn.Sequential(
-            nn.Linear(input_dim, input_dim),
+            nn.Linear(input_dim*2, input_dim),
             nn.Sigmoid()
         )
-
         self.text_gate = nn.Sequential(
-            nn.Linear(input_dim, input_dim),
+            nn.Linear(input_dim*2, input_dim),
             nn.Sigmoid()
         )
 
     def forward(self, image_embeddings, text_embeddings):
-        image_gate_values = self.image_gate(image_embeddings)
-        text_gate_values = self.text_gate(text_embeddings)
+        combined_embeddings = torch.cat([image_embeddings, text_embeddings], dim=1)
+        image_gate_values = self.image_gate(combined_embeddings)
+        text_gate_values = self.text_gate(combined_embeddings)
 
         gate_sum = image_gate_values + text_gate_values
 
