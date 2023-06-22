f3222e2533335e2c471ab6a6b6674d7d97207fc0
Ashley Holman + ChatGPT + Co-Pilot <ash@macbook>
2023-06-23 01:14:45 +0700

    fetch_datasets.py: Add new embeddings files.

--- src/scripts/fetch_datasets.py
+++ src/scripts/fetch_datasets.py
@@ -15,13 +15,17 @@ URLS = [
     "http://images.cocodataset.org/zips/train2014.zip",
     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip",
     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip",
-    "s3://vqa-ap-southeast-1/datasets/train_embeddings.pt",
+    "s3://vqa-ap-southeast-1/datasets/train_text_embeddings.bert-base-uncased.pt",
+    "s3://vqa-ap-southeast-1/datasets/train_vision_embeddings.vit-base-patch16-224-in21k.pt",
+    "s3://vqa-ap-southeast-1/datasets/train_vision_embeddings.vit-huge-patch14-224-in21k.pt",
 
     # Validation set
     "http://images.cocodataset.org/zips/val2014.zip",
     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip",
     "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip",
-    "s3://vqa-ap-southeast-1/datasets/validation_embeddings.pt"
+    "s3://vqa-ap-southeast-1/datasets/validation_text_embeddings.bert-base-uncased.pt",
+    "s3://vqa-ap-southeast-1/datasets/validation_vision_embeddings.vit-base-patch16-224-in21k.pt",
+    "s3://vqa-ap-southeast-1/datasets/validation_vision_embeddings.vit-huge-patch14-224-in21k.pt"
 ]
 
 def is_ec2():
